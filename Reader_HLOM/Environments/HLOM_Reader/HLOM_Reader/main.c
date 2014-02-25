48dd851b4842f8a9fe5eac69a400e40daa0b3d3a
Van Ly <van.ly@mail.com>
2014-02-25 17:56:53 +1100

    Xcode Init.

---
+++ Reader_HLOM/Environments/HLOM_Reader/HLOM_Reader/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  HLOM_Reader
+//
+//  Created by Van Ly on 25/02/2014.
+//  Copyright (c) 2014 Van Ly. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
